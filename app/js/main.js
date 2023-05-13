d054ba7c74938c816dd69c4b8d2c0b2cc258535e
GolovanovAlex <Golovanov.a.a@yandex.ru>
2023-05-13 21:50:11 +0300

    swiper

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,19 @@
-const swiper = new Swiper('.projects__swiper', {
+const swiper = new Swiper('.projects__swiper-text', {
+  slidesPerView: 3,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+const swiper2 = new Swiper('.projects__swiper', {
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
   pagination: {
     el: '.swiper-pagination',
+    clickable: true,
   },
   keyboard: true,
+  thumbs: {
+    swiper: swiper,
+  },
 });
